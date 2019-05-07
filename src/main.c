bb9ccc24e1d5742da4f481f0ecdcf17887e2048d
Tyler Filla <tylerfilla@hotmail.com>
2019-05-07 01:44:30 -0500

    Add linenoise dependency

--- src/main.c
+++ src/main.c
@@ -4,7 +4,31 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <linenoise.h>
 
 int main() {
-  printf("Hello, world!\n");
+  char* line;
+  while ((line = linenoise("> "))) {
+    if (!strcmp(line, "quit")) {
+      free(line);
+      break;
+    } else if (!strcmp(line, "auto on")) {
+      printf("request to enable automatic mode\n");
+    } else if (!strcmp(line, "auto off")) {
+      printf("request to disable automatic mode\n");
+    } else if (!strcmp(line, "start face-watcher")) {
+      printf("request to start faces diversion\n");
+    } else if (!strcmp(line, "start conversation")) {
+      printf("request to start conversation diversion\n");
+    } else if (!strcmp(line, "start wander")) {
+      printf("request to start wander diversion\n");
+    } else {
+      printf("Invalid command\n");
+    }
+
+    free(line);
+  }
 }
